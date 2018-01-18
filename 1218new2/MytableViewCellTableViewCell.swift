43ca032315c7fcd888d4d4a1220ad4332f5597d9
Yu-Liang Chou <yu-liangchou@yu-liangs-macbook-air.local>
2018-01-18 18:05:59 +0800

    1222

--- 1218new2/MytableViewCellTableViewCell.swift
+++ 1218new2/MytableViewCellTableViewCell.swift
@@ -8,30 +8,51 @@
 
 import UIKit
 
-class MytableViewCellTableViewCell: UITableViewCell {
+class MytableViewCellTableViewCell: UITableViewCell, UINavigationControllerDelegate, UIImagePickerControllerDelegate,UITextViewDelegate{
 
     @IBOutlet var profilePhoto: UIImageView!
     @IBOutlet var userName1: UILabel!
-    
     @IBOutlet var PostContent: UITextView!
+    @IBOutlet var PostPhoto: UIImageView!
     
     
-    @IBOutlet var postCell1: UIView!
+ 
     
-    @IBOutlet var postCell2: UIView!
+    var delegate:UIViewController?
+
+    @IBAction func postCamera(_ sender: Any) {
+        print("pick image")
+    let image = UIImagePickerController()
+        image.delegate = self
+        image.sourceType = UIImagePickerControllerSourceType.photoLibrary
+        image.allowsEditing = false
+        delegate?.present(image, animated: true
+            , completion: nil)
+    }
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [String : Any]) {
+        if let image = info[UIImagePickerControllerOriginalImage] as? UIImage
+        {
+            PostPhoto.image = image
+            delegate?.dismiss(animated: true, completion: nil)
+        }
+    }
+    
     
     
     override func awakeFromNib() {
         super.awakeFromNib()
         // Initialization code
 
-        PostContent.text = "Type in here"
-        PostContent.autocapitalizationType = UITextAutocapitalizationType.allCharacters
-        PostContent.inputAccessoryView = UITextView()
+        PostContent.text = "在想什麼?"
+        PostContent.delegate = self
+       
     }
     
-  
     
+    func textViewDidBeginEditing(_ textView: UITextView) {
+        textView.text = nil
+        //
+    }
 
     
     
--- PostContentController.swift
+++ PostContentController.swift
@@ -10,20 +10,21 @@ import Foundation
 import UIKit
 
 class PostContentController: UITableViewController{
+   
 
-
-    
-    
-    
-    
     
     override func viewDidLoad() {
         super.viewDidLoad()
+   
+        
         self.tableView.register(UINib(nibName: "MytableViewCellTableViewCell", bundle: nil), forCellReuseIdentifier: "CellID")
     }
     
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "CellID") as? MytableViewCellTableViewCell
+        cell?.delegate = self
+    
+//        將contoroller 放入cell的delegate中
         return cell!
         
     }
